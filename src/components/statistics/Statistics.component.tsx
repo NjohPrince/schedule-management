fb587de8bec67400d4941a82f10286f9967f874a
NjohPrince <jufredprince@gmail.com>
2022-10-24 09:00:04 +0100

    fixes

--- src/components/statistics/Statistics.component.tsx
+++ src/components/statistics/Statistics.component.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Grid from '../../layout/Grid.layout';
+import StatisticCardComponent from '../card/StatisticCard.component';
+
+// stylesheet
+import styles from './statistic.module.css';
 
 const StatisticsComponent = () => {
-    return <div>StatisticsComponent</div>;
+    return (
+        <div className={styles.statistics}>
+            <Grid column={3} gap={2} dropToOne={true}>
+                <StatisticCardComponent statHeadline="Missed" statValue="15" />
+                <StatisticCardComponent statHeadline="Rescheduled" statValue="21" />
+                <StatisticCardComponent statHeadline="Passed" statValue="05" />
+            </Grid>
+        </div>
+    );
 };
 
 export default StatisticsComponent;
